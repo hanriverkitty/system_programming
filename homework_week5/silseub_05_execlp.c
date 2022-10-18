4ec6d83e2346c25f20f751968f2e9226f67d139f
chan <danieltls@naver.com>
2022-10-18 19:51:02 +0900

    solve back search

--- homework_week5/silseub_05_execlp.c
+++ homework_week5/silseub_05_execlp.c
@@ -11,24 +11,23 @@
 #define MAXFILENAME 1024
 #define MAXPATHSTR 5000
 
-
 extern char **environ;
 int myexeclp(const char *file, const char *args, ...);
 int check(char *pathnames, char *file);
-char answer[MAXPATHSTR]="";
+char answer[MAXPATHSTR] = "";
 int main(void) {
   char path[MAXPATHSTR];
   sprintf(path, "PATH=%s:%s", getcwd(NULL, 0), getenv("PATH"));
   putenv(path);
   // prepare the executable file named "hello"
   system("cp hello.tmp hello.c");
-  system("gcc -pthread -lm -o hello hello.c");
+  system("clang -pthread -lm -o hello hello.c");
   system("rm hello.c");
   // char *file_dir=strcat(getcwd(NULL,0),"/hello");
   // char *exam[] = {file_dir,"-a","-b","-c",NULL};
   // char *pathvar[] = {getenv("PATH"), NULL};
   // execve(file_dir, exam, pathvar);
-  printf("%d\n",myexeclp("hello", "-a", "-b", "-c", (char *)0));
+  printf("%d\n", myexeclp("hello", "-a", "-b", "-c", (char *)0));
 
   return 0;
 }
@@ -62,60 +61,78 @@ int myexeclp(const char *file, const char *args, ...) {
   strcat(file_dir, "/");
   strcat(file_dir, file);
 
-  if (check(pathvar[0], (char *)file)==2) {
-	  //복사된 경로로 실행
+  if (check(pathvar[0], (char *)file) == 2) {
+    //복사된 경로로 실행
     if (execve(answer, vector, pathvar) < 0) {
       fprintf(stderr, "execve error\n");
       exit(-1);
     }
-  } 
-  else{
+  } else {
     return -1;
-	  }
+  }
   return 0;
 }
 
 int check(char *pathnames, char *file) {
   char pathname[MAXPATHSTR];
-//콜론 기준으로 나누기
+  char copy_pathnames[MAXPATHSTR];
+  strcpy(copy_pathnames, pathnames);
+
+  int cnt = 0;
+
+  char *ptr1 = strtok(copy_pathnames, ":");
+  //콜론 기준으로 몇개인지 검색
+  while (ptr1 != NULL) {
+    cnt++;
+    ptr1 = strtok(NULL, ":");
+  }
+  // printf("%d\n", cnt);
+  char copy_pathname[cnt][MAXPATHSTR];
+  //콜론 기준으로 나누기
   char *ptr = strtok(pathnames, ":");
-	int i =0;
+  int i = 0;
+  int j = cnt;
+
+  //배열뒤쪽부터 경로를 저장
   while (ptr != NULL) {
     strcpy(pathname, ptr);
     strcat(pathname, "/");
     strcat(pathname, file);
-    printf("%s\n", pathname);
-	// strcpy(copy_pathname[i] ,(pathname));
-	// i++;
-
-	  //나눈 콜론으로 파일이 있는지, 읽기권한이 있는지 확인후 있으면 2 return
-    if (access(pathname, F_OK) < 0) {
-      ptr = strtok(NULL, ":");
+    // printf("%s\n", pathname);
+    strcpy(copy_pathname[j - 1], (pathname));
+    j--;
+    ptr = strtok(NULL, ":");
+  }
+  // home/runner/silseub-0-execlp-gunheonhagi-ShinChanKyu/hello
+  while (i <= cnt) {
+    // printf("%s\n",copy_pathname[i]);
+    //나눈 콜론으로 파일이 있는지, 읽기권한이 있는지 확인후 있으면 2 return
+    if (access(copy_pathname[i], F_OK) < 0) {
+      i++;
       continue;
     } else {
       mode_t file_mode;
       struct stat file_info;
       int return_stat;
-		
-      if ((return_stat = stat(pathname, &file_info)) == -1) {
+
+      if ((return_stat = stat(copy_pathname[i], &file_info)) == -1) {
         perror("Error : ");
         exit(0);
       }
       file_mode = file_info.st_mode;
-		//읽기권한이 있는지
+      //읽기권한이 있는지
       if ((file_mode & S_IXUSR) == 0) {
-        ptr = strtok(NULL, ":");
         printf("No permission\n");
         exit(-1);
       } else {
-        //printf("%s\n", pathname);
-		//pathname 복사
-		strcpy(answer,pathname);
-		//printf("%s\n",answer);
-		return 2;
+        printf("%s\n", copy_pathname[i]);
+        // pathname 복사
+        strcpy(answer, copy_pathname[i]);
+        // printf("%s\n",answer);
+        return 2;
       }
     }
-	  	ptr = strtok(NULL, ":");
+    i++;
   }
-	return 0;
+  return 0;
 }
