478e98f6138464b4c0152f61f11fb6e08c7def71
chan <danieltls@naver.com>
2022-12-01 16:16:04 +0900

    history

--- shell_project/main.c
+++ shell_project/main.c
@@ -1,5 +1,6 @@
 #include <dirent.h>
 #include <fcntl.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,126 +8,124 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <pwd.h>
 
-char buf[255];
-char *cur_dir(char *buf1, size_t size);
 char *get_usr_name();
 char *current_name = "";
+void erase_space(char *a);
 char *dir;
+char command[7][3] = {{">"},{">>"},{">|"},{"&"},{";"},{"<"},{"|"}};
+
 
 int main(void) {
-	char *user = get_usr_name();
- 
+  char *user = get_usr_name();
+
   while (1) {
+    char buf[255];
     char buf1[255];
-	printf("%s:%s$ ",user,cur_dir(buf1,255));
-	fgets(buf,255,stdin);
-  }
-}
-
-char *get_usr_name(){
-	int a,b,c;
-	if((a=open("get_usr_name",O_CREAT,0600))==-1){
-		perror("open");
-		exit(1);
-	}
-	struct stat info;
-	char *user_name;
-	struct passwd *my_passwd;
-	if((b=stat("get_usr_name",&info))==-1){
-		perror("stat error");
-		exit(1);
-	}
-	my_passwd = getpwuid(info.st_uid);
-	user_name = my_passwd->pw_name;
-	if((c = remove("get_usr_name"))==-1){
-		perror("remove error");
-		exit(1);
-	}
-	return user_name;
-}
-
-
-char *cur_dir(char *buf1, size_t size) {
-
-  char file_name[255] = ".";
-  struct stat file_info;
-  mode_t file_mode;
-  ino_t inode;
-  ino_t root_inode;
-  ino_t search_inode;
-  struct stat root_info;
-  struct stat search_info;
-  int return_stat, return_stat1, return_stat2;
-  char search_dir[255] = "..";
-  char dir_name[255] = "";
-
-  //루트 디렉토리의 inode확인
-  if ((return_stat1 = stat("/", &root_info)) == -1) {
-    perror("Error : ");
-    exit(0);
-  }
-  root_inode = root_info.st_ino;
-  // printf("%ld\n",root_inode);
-
-  struct dirent *item;
-  DIR *dp;
-  struct dirent *root_item;
-  DIR *root_dp;
-
-  root_dp = opendir("/");
-  if (root_dp != NULL) {
-    for (;;) {
-      root_item = readdir(root_dp);
-      if (root_item == NULL)
-        break;
-      // printf("%s  %ld\n", root_item->d_name,root_item->d_ino);
+    int m = 0;
+    printf("%s:%s$ ", user, getcwd(NULL, 0));
+    fgets(buf, 255, stdin);
+    /* history 파일에 명령어 추가 */
+    if(buf[0]!='\n'){
+    int his_fd;
+    if((his_fd = open("history",O_CREAT|O_WRONLY|O_APPEND,0600))<0){
+      perror("open err");
+      exit(1);
     }
-    closedir(root_dp);
-  }
-
-  for (;;) {
-    //현재디렉토리 inode 확인
-    if ((return_stat2 = stat(file_name, &search_info)) == -1) {
-      perror("Error : ");
-      exit(0);
+    if((write(his_fd,buf,strlen(buf)))!=strlen(buf)){
+      perror("write err");
+      exit(1);
     }
-    search_inode = search_info.st_ino;
+    close(his_fd);
+    }
 
-    // printf("%ld\n",search_inode);
+    
+    buf[strlen(buf)-1]='\0';
 
-    //루트디렉토리랑 같을경우 for문 빠져나오기
+    /*공백제거*/
+    erase_space(buf);
+    printf("%s\n", buf);
+    char a[255];
+    strcpy(a,buf);
 
-    if (search_inode == root_inode)
-      break;
-
-    dp = opendir(search_dir);
-    if (dp != NULL) {
-      for (;;) {
-        item = readdir(dp);
-        if (item == NULL)
-          break;
-        // printf("%s  %ld\n", item->d_name,item->d_ino);
-        if (search_inode == item->d_ino) {
-          current_name = item->d_name;
-          // printf("find direct %s\n",item->d_name);
+    if(strcmp(buf,"history")==0){
+      int his_fd1;
+      int n;
+      int i=1;
+      char read_buf[255];
+      
+      his_fd1=open("history",O_RDONLY);
+      while(n=read(his_fd1,read_buf,255)>0){
+          char *ptr = strtok(read_buf, "\n");    
+          while (ptr != NULL){
+            printf("\t\t%d\t%s\n", i,ptr);         
+            ptr = strtok(NULL, "\n");
+            i++; 
+          }
         }
-      }
-      closedir(dp);
+      close(his_fd1);
     }
-    // printf("%s\n\n",current_name);
-    //현재의 경로에서 부모경로로 경로 추가
-    strcat(search_dir, "/..");
-    strcat(file_name, "/..");
-    char temporary_string[255];
-    //파일 경로 순서 바꿔서 저장
-    strcpy(temporary_string, "/");
-    strcat(temporary_string, current_name);
-    strcat(temporary_string, dir_name);
-    strcpy(dir_name, temporary_string);
+    // char *sear = strchr(buf, ';');
+    // if (sear != NULL) {
+    //   printf("%s\n", sear);
+    //   sear = strtok(NULL, ";");
+    // }
+ //    char *mode = strtok(buf, "><|&");
+	//   printf("%s\n",mode);
+	// if(mode==NULL){
+	// 	m=10;
+	// }
+	// mode = strchr(buf,'>');
+ //    if (mode != NULL) {
+	// 	printf("%s\n",mode);
+ //      if (*(mode + 1) == '>') {
+ //        m = 1;
+	// 	char *mode = strtok(buf,">>");
+ //      } else if (*(mode + 1) == '|') {
+ //        m = 2;
+	// 	char *mode = strtok(buf,">|");
+ //      }
+ //    }
+	// mode = strchr(buf, '<');
+	//   if(mode !=NULL){
+	// 	  m=3;
+	//   }
+	   printf("%d\n",m);
   }
-  // printf("%s\n",answer);
-  strcpy(buf1, dir_name);
-  return buf1;
+}
+
+char *get_usr_name() {
+  int a, b, c;
+  if ((a = open("get_usr_name", O_CREAT, 0600)) == -1) {
+    perror("open");
+    exit(1);
+  }
+  struct stat info;
+  char *user_name;
+  struct passwd *my_passwd;
+  if ((b = stat("get_usr_name", &info)) == -1) {
+    perror("stat error");
+    exit(1);
+  }
+  my_passwd = getpwuid(info.st_uid);
+  user_name = my_passwd->pw_name;
+  if ((c = remove("get_usr_name")) == -1) {
+    perror("remove error");
+    exit(1);
+  }
+  return user_name;
+}
+
+void erase_space(char *buf) {
+  char *a = buf;
+  int b = 0;
+
+  while (*buf != 0) {
+    if (*buf != ' ') {
+      *a = *buf;
+      a++;
+    }
+    buf++;
+  }
+  *a = 0;
 }
